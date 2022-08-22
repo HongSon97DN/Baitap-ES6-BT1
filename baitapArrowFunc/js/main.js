14eb4dc3bb43852a3eeec209ba1095bd566943dd
Son <phanhongson220297@gmail.com>
2022-08-22 22:38:42 +0700

    btvn

---
+++ baitapArrowFunc/js/main.js
@@ -0,0 +1,9 @@
+
+const colorList = ["pallet","viridian","pewter","cerulean","vermillion","lavender","celadon","saffron","fuschia","cinnabar"];
+
+let showButton = () => {
+    let content = "";
+    colorList.map(color,index =>{
+        
+    })
+}
