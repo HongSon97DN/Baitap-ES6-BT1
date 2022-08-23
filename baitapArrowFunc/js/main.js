77108fd35f1e82c288140409e8ebb81774fedc85
Son <phanhongson220297@gmail.com>
2022-08-23 15:47:29 +0700

    update bt arrowfunc

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -3,7 +3,30 @@ const colorList = ["pallet","viridian","pewter","cerulean","vermillion","lavende
 
 let showButton = () => {
     let content = "";
-    colorList.map(color,index =>{
+    colorList.map((color,index) =>{
+        if (index == 0) {
+            content += `
+            <button class="color-button active ${color}" onclick="changeColor('${color}')"></button>
+        `
+        } else{
+            content += `
+            <button class="color-button ${color}" onclick="changeColor('${color}')"></button>
+        `
+        }
         
     })
+    document.getElementById("colorContainer").innerHTML = content;
+}
+showButton();
+
+let changeColor = (color) => {
+    document.getElementById("house").className = `house ${color}`; 
+    
+    let btnNodeList = document.querySelectorAll("#colorContainer .color-button");
+ 
+    for (let i = 0; i < btnNodeList.length; i++) {
+        btnNodeList[i].classList.remove("active");
+    }
+    
+    document.querySelector(`#colorContainer .${color}`).className = `color-button ${color} active`;
 }
